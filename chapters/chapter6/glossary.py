1e8624d226f38e9c5b603fe0878fa68c7d3a1a6c
ryanmichael <112053375+conspirathon@users.noreply.github.com>
2024-07-20 00:41:10 -0400

    Updated glossary.py: with 6-4 challenge

--- chapters/chapter6/glossary.py
+++ chapters/chapter6/glossary.py
@@ -7,5 +7,12 @@ dict_glossary["lists"] = "Ordered collections of items, defined by '[]'."
 dict_glossary["loops"] = "Structures that repeat a block of code multiple times."
 dict_glossary["functions"] = "Blocks of code designed to preform a specific task."
 
+# 6-4. Glossary 2:
+dict_glossary["int"] = "Integer number."
+dict_glossary["boolean"] = "True or False value."
+dict_glossary["set"] = "Unordered collection of unique items."
+dict_glossary["dict"] = "Key-value pairs collection."
+dict_glossary["tuple"] = "Immutable ordered sequence."
+
 for key, value in dict_glossary.items():
     print(f"\n{key.title()}:\n\t{value}")
